2824b5a1ee2beea091a552e8ef50f29b166b4a7d
007khun159 <008khun159@gmail.com>
2023-02-11 15:16:20 +0700

    add_CRU

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request,url_for,session,redirect
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
-import os
 
 
 
@@ -19,97 +18,97 @@ app.config['MYSQL_DB'] = 'fitness'
 #Intialize mysql
 mysql = MySQL(app)
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
 
-     #the output for show message 
+
+
+
+@app.route('/' , methods = ['GET', 'POST'])
+def login():
     msg = ''
-    #Authenticating Users 
-    #the method for check username 
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        #create variable for access
+    # check if username and password POST {request  exit user submit from}
+    if request.method == "POST" and "username" in request.form  and 'password' in request.form:
+        #Create variable for easy acess
         username = request.form['username']
         password = request.form['password']
 
-        #check account exits using Mysql
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-
-        # Fetch one record and return result
+        #check if accoutnt exists using in mysql 
+        # MySQLdb.cursors.DictCursor เราจะใช้ก็ต่อเมื่อค่าของ input ที่รับเข้ามานั้นมี type ที่แตกต่างกัน 
+        cursor  = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM fitness.member WHERE user = %s and password = %s" ,(username,password))
         account = cursor.fetchone()
-    #If account exits in accoutn  table in database 
-        if account : 
-            #Create data session   
+
+
+
+        #if account exists in accounts table in out database 
+        if account:
+        # Create session data , for acess this  data in other route
             session['loggedin'] = True
             session['id'] = account['id']
-            session[username]= account['username']
-            # Redirect to homepage 
-            return 'Logged in successfully'
+            #we will compare  user in mysql and session input form html 
+            session['username'] = account['user']
 
-        else:
-            #Account doesn't exit from DB
-            msg = 'Incorrect username/password!'
+            # Redirect to homepage
 
-    return render_template('index.html',msg=msg)
+            return 'Logged in successfully!!'
+
+
+        else: 
+            # if account is not exit 
+            msg = 'Incorrect Username/Password'
+
+    return render_template('index.html', msg = msg)
+
+
 
 
 @app.route('/logout')
 def logout():
 
-    session.pop('loggedin',None)
+    #Remove session data , this will log the userout
+    session.pop('loggedin' , None)
     session.pop('id',None)
-    session.pop('username',None)
-    # Getout to login page
+    session.pop('username' , None)
 
+    # ทำการ redirect หน้าออกไป
     return redirect(url_for('login'))
-
-
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        
-        usernames = request.form['username']
+    if request.method == "POST":
+        username = request.form['username']
         password = request.form['password']
-        email = request.form['email']
-    
-    elif request.method == 'POST':
-    
-        msg = 'Please fill out the form!'
-    
-    #check if account exits using Mysql
+        email  = request.form['email']
+        #ทำการเช็คข้อมูลว่า มีปัญาตนงไหนเช่นการซ้ำ username password email
+        if not username or not password or not email:
+            msg = 'Please fill out all the fields!'
 
-    # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    # cursor.execute('SELECT * FROM accounts WHERE username = %s', (usernames,))
-    # account = cursor.fetchone()
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
 
-    # if account :
-    #     msg = "Account already exists!!"
-    
-    # elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-    #     msg = "Invalid email address!!"
-    
-    # elif not re.match(r'[A-Za-z0-9]+',usernames):
-    #     msg ='Username must contain only characters and numbers!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
 
-    # elif not usernames or not password or not email:
-    #     msg = 'Please fill the from '
+        else:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT * FROM fitness.member_test WHERE user = %s", (username,))
+            account = cursor.fetchone()
 
-
-    # else: 
-    #     cursor('INSEART INTO accounts VALUES (NULL %s, %s, %s )',(usernames,password,email))
-    #     mysql.connect.commit()
-    #     msg = "You have  successfully registered !!"
-
+            if account:
+                msg = 'An account with this username already exists!'
+            else:
+                cursor.execute('INSERT INTO fitness.member_test VALUES (NULL, %s, %s, %s)', (username, password, email))
+                mysql.connection.commit()
+                msg = 'Account created successfully!'
 
     return render_template('register.html', msg=msg)
 
+    
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
 
+
+
+if __name__ == "__main__":
+    app.run(debug= True)
